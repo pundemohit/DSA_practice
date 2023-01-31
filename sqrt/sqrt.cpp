22e00a3660c13dfecc91f2e55772cae8a37e1477
Mohit <pundemohit@gmail.com>
2023-01-31 13:35:35 +0530

    Commit

---
+++ sqrt/sqrt.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+ long long int sqrtInt(int n){
+
+        int s= 0;
+        int e = n;
+        long long int mid = s + (e-s)/2;
+        long long int ans = -1;
+
+        while(s<=e){
+            long long int square = mid * mid;
+
+            if(square == n){
+                return mid;
+            }
+            if(square<n){
+                ans= mid;
+                s = mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+            mid = s + (e-s)/2;
+        }
+        return ans;
+    }
+    
+    double morePre( int n, int pre, int tempSol){
+    	
+    	double factor = 1;
+    	double ans = tempSol;
+    	
+    	for(int i =0; i<pre; i++){
+    		factor = factor /10;
+    		for(double j = ans; j*j<n; j=j+factor){
+    			ans= j;
+			}
+    		
+		}
+		return ans;
+    	
+	}
+    
+    int main(){
+    	int n;
+    	cout<<"enter the number: "<<endl;
+    	cin>>n;
+    	int tempSol = sqrtInt(n);
+    	
+    	cout<<"Answer is "<< morePre(n, 3, tempSol)<<endl;
+    	
+    	
+    	
+    	return 0;
+    	
+	}
