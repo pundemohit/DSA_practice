7d00fe688d464b5b99cfbb2d409c87aab01bc699
Mohit <pundemohit@gmail.com>
2023-02-07 19:55:37 +0530

    Ver-0.1

---
+++ array/movingZeros.cpp
@@ -0,0 +1,18 @@
+#include<vector>;
+#include<iostream>;
+using namespace std;
+
+// Only CLASS FUNCTION FOR MOVING ZEROS **
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        for(int j =0; j<nums.size(); j++){
+            
+            if(nums[j]!=0){
+                swap(nums[j], nums[i]);
+                i++;
+            }
+        }
+    }
+};
