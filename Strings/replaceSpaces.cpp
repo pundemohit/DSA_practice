3a38c3962ab797bf4268a59cc201ee89532cabcd
Mohit <pundemohit@gmail.com>
2023-02-16 22:46:54 +0530

    no-0.8

---
+++ Strings/replaceSpaces.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string replaceSpaces(string &str){
+	string temp = "";
+    
+    for(int i=0; i<str.length(); i++){
+        if(str[i] == ' '){
+            temp.push_back('@');
+            temp.push_back('4');
+            temp.push_back('0');
+        }
+        else
+        {
+            temp.push_back(str[i]);
+        }
+    }
+    return temp;
+}
+int main()
+{
+    string s;
+    cin >>s;
+    cout<<replaceSpaces(s)<<endl;
+}
