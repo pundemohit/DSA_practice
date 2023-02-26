b2c1e307faa7a26111c7dcd6bd319023ca491714
Mohit <pundemohit@gmail.com>
2023-02-26 22:39:54 +0530

    no-0.15

---
+++ Pointers/doublePointer.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+void update(int **p){
+    p = p+1;
+
+    *p = *p +1;
+
+    **p =  **p +1;
+}
+
+int main(){
+
+    int i =5;
+    int*p = &i;
+    int **p2 = &p;
+
+    cout<<endl<<endl<<" Chala hawa yeun dyaa"<<endl;
+    // cout<<endl<<"Printing p : "<< p<<endl;
+    // cout<<endl<<"Address of p"<< &p<<endl;
+
+    // cout<<i <<endl;
+    // cout<<*p <<endl;
+    // cout<<**p2 <<endl;
+    // void update();
+    // cout<<&i <<endl;
+    // cout<<p <<endl;
+    // cout<<*p2 <<endl;
+    
+    cout<<i <<endl;
+    cout<<p <<endl;
+    cout<<p2 <<endl;
+    update(p2);
+    cout<<i <<endl;
+    cout<<p <<endl;
+    cout<<p2 <<endl;
+    return 0;
+}
--- Pointers/functions_with_pointer.cpp
+++ Pointers/functions_with_pointer.cpp
@@ -16,9 +16,9 @@ int getSUM(int arr[], int n){
     int sum =0;
     for (int i = 0; i < n; i++)
     {
-        
+        sum = sum+arr[i];
     }
-    
+    return sum;
 }
 
 int main(){
