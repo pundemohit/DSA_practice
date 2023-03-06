3956707a105149f24cf910d0421e02eec464102f
Mohit <pundemohit@gmail.com>
2023-03-06 22:48:38 +0530

    no-0.17

--- sorting/bubbleSort/bubbleSort.cpp
+++ sorting/bubbleSort/bubbleSort.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void bubble(int arr[], int n){
 	for(int i = 0; i<n; i++){
 
-        for(int j =0; j<n-i; j++){
+        for(int j =0; j<n-i -1; j++){
             if(arr[j]> arr[j+1]){
                 swap(arr[j], arr[j+1]);
             }
