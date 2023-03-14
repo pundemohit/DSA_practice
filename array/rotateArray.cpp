ac4e2cbf99e3673aacb37b63079d979c1f4a5948
Mohit <pundemohit@gmail.com>
2023-03-14 22:11:29 +0530

    Code Challenge

---
+++ array/rotateArray.cpp
@@ -0,0 +1,14 @@
+//sol class/func
+#include<vector>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k %=nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
+    }
+};
