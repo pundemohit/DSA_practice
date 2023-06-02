cf219beb9ae7af204ab9f41a42ca74e908be240a
Mohit <pundemohit@gmail.com>
2023-06-02 20:39:54 +0530

    ...

--- Linked List/singleLinkedList.cpp
+++ Linked List/singleLinkedList.cpp
@@ -32,6 +32,40 @@ void print(Node* &head){
     cout<<endl;
 }
 
+void InsertAtTail(Node* &tail, int d){
+    Node* temp = new Node(d);
+    tail->next = temp;
+    tail = tail->next; 
+
+}
+
+void InsertAtPosition(Node* &tail,Node* &head, int position, int d ){
+
+    //insert at start
+    if(position ==1){
+        InsertAtHead(head, d);
+    }
+
+    Node* temp = head;
+    int cnt = 1;
+    while(cnt < position -1){
+        temp = temp->next;
+        cnt++;
+    }
+    //inserting at last
+    if(temp -> next = NULL){
+        InsertAtTail(tail,d);
+    }
+
+    //creating a node for d
+    Node* nodeToInsert = new Node(d);
+
+    nodeToInsert-> next = temp->next;
+    temp->next = nodeToInsert;
+
+
+}
+
 int main(){
 
     // creating new node
@@ -40,12 +74,23 @@ int main(){
     // cout<<node1->next<<endl;
 
     Node* head = node1;
+    Node* tail = node1;
 
+    print(head);
+
+    InsertAtTail(tail, 100);
     print(head);
 
     InsertAtHead(head, 12);
+
     print(head);
     InsertAtHead(head, 99);
     print(head);
+
+    InsertAtPosition(tail, head, 3, 1000);
+    print(head);
+
+    cout<<"head" << head -> data << endl;
+    cout<<"tail" << tail -> data <<endl;
     return 0;
 }
